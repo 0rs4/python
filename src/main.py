b168234b37899af09e1a413bf7de9d2813436e2f
root <root@localhost>
2023-03-06 19:50:07 +0530

    auto publish 1678112407075

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Emma Watson")
+def count_vowels(sentence):
+  count = 0
+  vowels = ['a','e','i','o','u','A','E','I','O','U']
+  for char in sentence:
+     if char in vowels:
+        count += 1
+  return count
+
+print(count_vowels("Hello World"))
