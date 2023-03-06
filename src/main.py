48c2c25a90eec9b49f8a2caa4228a621194f1e89
root <root@localhost>
2023-03-06 19:28:32 +0530

    auto publish 1678111111727

--- src/main.py
+++ src/main.py
@@ -1,9 +1,1 @@
-text = 'Love to watching Movie'
-count = 0
-for char in text:
-   if char == ' ':
-       count = count + 1
-#add one for the last word
-count = count + 1
-
-print(count)
+print("Emma Watson")
